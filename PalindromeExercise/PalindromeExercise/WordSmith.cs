19312f4b78b13e051d251b8953bafb79a4cdeaf8
elimarvin99 <elimarvin99@gmail.com>
2021-11-17 14:43:54 -0500

    finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        { 
+        }
+        public bool IsPalindrome(string s)
+        {
+            return s.SequenceEqual(s.Reverse());
+            //var original = s;
+            //var reversed = new string(original.Reverse().ToArray());
+            //var palindrom = original == reversed;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("basketball", false)]
+        [InlineData("redivider", true)]
+        [InlineData("reviver", true)]
+        [InlineData("Apples", false)]
+        public void Test1(string s, bool expected)
         {
+            // Arrange
+            var test = new WordSmith();
+            // Act
+            var actual = test.IsPalindrome(s);
 
+            // Assert
+            Assert.Equal(actual,expected);
         }
     }
 }
